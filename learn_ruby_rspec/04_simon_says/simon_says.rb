faabfe84fca254b5054a07d055d584d8ef5fab8e
CapellutoY <yossi.capelluto@gmail.com>
2018-10-08 23:25:24 +0200

    last commit

--- learn_ruby_rspec/04_simon_says/simon_says.rb
+++ learn_ruby_rspec/04_simon_says/simon_says.rb
@@ -11,3 +11,19 @@ def repeat(string, n = 2)
    s = "#{string} " * n
    s.chop #vire l'espace de fin
 end
+
+def start_of_word(name,n)
+    n= n-1
+    p name.slice(0..n)
+end
+def first_word (words)
+ p words.split.first
+end
+def titleize(string)
+    no_cap = ["and", "or", "the", "to", "the", "a", "but", "in"]
+
+    title = string[0].capitalize + string.split(" ").map { |word| no_cap.include?(word) ? word :
+    word.capitalize }.join(" ")
+    p title[0...1]+title[2..-1]
+
+end
